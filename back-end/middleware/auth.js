8992065f3d52162103d45e03291902ccf41e5b97
Fezeee <farukibrahim913@gmail.com>
2024-12-27 15:36:31 +0100

    debugging...

--- back-end/middleware/auth.js
+++ back-end/middleware/auth.js
@@ -1,17 +1,13 @@
 import jwt from 'jsonwebtoken'
  
 const authUser = async(req, res, next) => {
-    const secret = process.env.JWT_SECRET;
-    if (!secret) {
-        throw new Error("SECRET environment variable is not defined");
-    }
     const { token } = req.headers
     if(!token){
         return res.json({success: false, message:"Not authorized, Login again"})
     }
 
     try {
-        const token_decode = jwt.verify(token, process.env.JWT_secret)
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET)
         req.body.userId = token_decode.id
         next();
     } catch (error) {
